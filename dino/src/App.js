3ecf344f04d0d01da82dd1cef21d5f4ec40b6093
jeff indall <jeffindall@gmail.com>
2018-05-09 14:59:10 -0600

    first commit

--- dino/src/App.js
+++ dino/src/App.js
@@ -1,21 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+ import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import JobList from './components/JobList'
+import JobForm from './components/JobForm'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      jobs:[],
+    }
+  }
+  componentDidMount(){
+    fetch("./listings.json")
+      .then(response => response.json())
+      .then(jobs => {
+        return this.setState({
+        jobs: jobs
+           
+        })
+         
+      })
+     
+  }
+  submitJobListing = (job) => {
+    this.setState({
+      jobs: this.state.jobs.concat(job).reverse()
+    })
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      
+        <Header />
+        <main>
+          <JobList jobs={this.state.jobs} />
+          <JobForm submitJobListing={this.submitJobListing}/>
+        </main>
+        
+        <Footer />
       </div>
     );
   }
 }
 
 export default App;
+
+
---
+++ dino/src/components/JobForm.js
@@ -0,0 +1,49 @@
+import React from "react"
+
+
+class JobForm extends React.Component{
+    state = {
+        title: '',
+        pay:  '',
+        description: '',
+        interested: ''
+    }
+    handleUserInput = (event) => {
+        this.setState ({[event.target.name]: event.target.value})
+    }
+    resetForm = () => {
+        this.setState ({
+            title: '',
+            pay:  '',
+            description: '',
+            interested: ''
+        })
+    }
+    handleFormSubmit = (event) => {
+        event.preventDefault()
+            this.props.submitJobListing(this.state)
+            this.resetForm()
+    }
+    render(){
+        return(
+        <aside id="side-bar">
+        <h3>Add a Job</h3>
+            <form className="job-form" onSubmit = {this.handleFormSubmit}>
+                <label htmlFor="title">Title</label>
+                <input type="text" name="title" value={this.state.title} onChange= {this.handleUserInput} />
+                <label htmlFor="pay">Compensation</label>
+                <input type="text" name="pay"  value={this.state.pay} onChange= {this.handleUserInput} />
+                <label htmlFor="description">Description</label>
+                <textarea name="description"  value={this.state.description} onChange= {this.handleUserInput} rows="8" cols="40"></textarea>
+                <input type="submit" name="submit" value="Submit" />
+            </form>
+        </aside>
+    )
+    }
+}  
+   
+  
+  
+
+
+export default JobForm
